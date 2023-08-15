658ed82f086d7ed25a5b279fe1ebf9ed0ad06cd0
Jordan Kay <jordan@fleuronic.com>
2023-08-15 11:35:53 -0400

    Update package

--- Package.swift
+++ Package.swift
@@ -20,6 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Yaml",
-            dependencies: [])
+            dependencies: []),
+		.testTarget(
+			name: "YamlTests",
+			dependencies: [])
     ]
 )
